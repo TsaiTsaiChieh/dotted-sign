b1d1b3baa54dc0fb2461f46962cc395786905a96
Tsai-Chieh <d0177381@gmail.com>
2022-11-13 02:12:19 +0800

    :tada: init Home component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import Background from './Background'
+import Header from './Header'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Background />
+    </>
+  )
+}
+
+export default Home
