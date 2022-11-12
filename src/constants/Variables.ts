b3a666b97cbcf2a93af8d1d449eb937e88244b43
Tsai-Chieh <d0177381@gmail.com>
2022-11-13 01:34:46 +0800

    :tada: init Variable for style using

---
+++ src/constants/Variables.ts
@@ -0,0 +1,17 @@
+export const Color = {
+  primary: {
+    '50': '#FAC200',
+    '100': '#FFE100',
+    '150': '#FFFCDB',
+  },
+  secondary: {
+    '50': '#964900',
+  },
+  black: {
+    '50': '#231815',
+  },
+  blue: {
+    '50': '#2FBDF0',
+    '100': '#B0DFF8',
+  },
+} as const
