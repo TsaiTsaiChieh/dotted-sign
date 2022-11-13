6a58fbf09dcc78d17945654c435ca8ef4f31192d
Tsai-Chieh <d0177381@gmail.com>
2022-11-14 00:35:32 +0800

    :sparkles: add Banner styled-components and HomeContainer

--- src/styled/Home.ts
+++ src/styled/Home.ts
@@ -2,6 +2,17 @@ import styled from 'styled-components'
 
 import {Color} from '../constants/Variables'
 
+export const HomeContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  background: ${Color.primary[150]};
+  img.bg {
+    position: absolute;
+    z-index: 1;
+  }
+`
+// Navbar
 export const Navbar = styled.nav`
   width: 100%;
   background: ${Color.secondary['50-a70']};
@@ -27,13 +38,38 @@ export const Logo = styled.img`
   height: 43px;
   content: url("/images/logo.svg");
 `
-export const BG = styled.div`
+// Banner
+export const BannerWrap = styled.div`
   width: 100%;
+  display: flex;
   height: 100vh;
-  z-index: -1;
-  position: relative;
-  background: ${Color.primary[150]};
-  img {
-    position: absolute;
-  }
+`
+export const LeftSide = styled.div`
+  width: 35%;
+  display: flex;
+  padding-left: 15%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const Title = styled.img`
+  width: 334px;
+  height: 104px;
+  content: url("/images/word.svg");
+  z-index: 10;
+`
+export const SubTitle = styled.h1`
+  color: black;
+  font-size: 38.74px;
+  margin: 32px 0 48px;
+`
+export const RightSide = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+`
+export const Bee = styled.img`
+  width: 100%;
+  height: 327px;
+  content: url("/images/bee-with-line.svg");
 `
