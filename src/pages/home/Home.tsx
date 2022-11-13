4972bfd4ddd6a0d98ef4f63af1a6ef5442101979
Tsai-Chieh <d0177381@gmail.com>
2022-11-14 00:36:11 +0800

    :sparkles: import Banner and HomeContainer

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,15 @@
+import {HomeContainer} from '../../styled/Home'
 import Background from './Background'
+import Banner from './Banner'
 import Header from './Header'
 
 const Home = () => {
   return (
-    <>
+    <HomeContainer>
       <Header />
+      <Banner />
       <Background />
-    </>
+    </HomeContainer>
   )
 }
 
