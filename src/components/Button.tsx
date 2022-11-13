4bef1e0354be09c714a35787eaf511728d0df552
Tsai-Chieh <d0177381@gmail.com>
2022-11-14 00:33:22 +0800

    :sparkles: add more Button settings

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,25 @@
+import {Color} from '../constants/Variables'
 import {BTN} from '../styled/Component'
 
 interface Props {
-  bg?: string
+  style?: string
   content: string
+  padding?: string
+  fz?: number
 }
-const Button = ({bg, content}: Props) => {
-  return <BTN bg={bg}>{content}</BTN>
+const Button = ({style = 'main', content, padding, fz}: Props) => {
+  let bg: string = Color.primary[50]
+  let color: string = Color.black[100]
+  if (style === 'main-reverse') color = Color.black[50] as string
+  if (style === 'white') {
+    bg = Color.white[50]
+    color = Color.secondary[50]
+  }
+  return (
+    <BTN bg={bg} color={color} padding={padding} fz={fz}>
+      {content}
+    </BTN>
+  )
 }
 
 export default Button
--- src/styled/Component.ts
+++ src/styled/Component.ts
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
 
-import {Color} from '../constants/Variables'
-
-export const BTN = styled.button<{bg?: string}>`
-  width: 138px;
-  height: 45px;
-  background: white;
-  color: ${Color.secondary[50]};
+export const BTN = styled.button<{
+  bg: string
+  color: string
+  padding?: string
+  fz?: number
+}>`
+  padding: ${(props) => (props.padding ? `${props.padding}` : '21px 72px')};
+  font-size: ${(props) => (props.fz ? `${props.fz}px` : '24px')};
+  background: ${(props) => props.bg};
+  color: ${(props) => props.color};
   border-radius: 3px;
-  font-size: 24px;
   font-weight: bold;
 `
