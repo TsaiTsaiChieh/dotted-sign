e641bb223459a22ed6a5e5b7bbad804d3dcf7ba3
Tsai-Chieh <d0177381@gmail.com>
2022-11-14 16:53:58 +0800

    :tada: init Auth styled-components

---
+++ src/styled/Auth.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const RegisterTitleWrap = styled.div`
+display: flex;
+align-items: center;
+gap: 13.75px;
+`
+export const RegisterTitle = styled.h2`
+font-weight: bold;
+font-size: 36px;
+`
+export const RegisterFormWrap = styled.div`
+display: flex;
+flex-direction: column;
+`
