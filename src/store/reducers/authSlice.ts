3fcb4eefed4fd74b817f15562c2b334493b450a4
Tsai-Chieh <d0177381@gmail.com>
2022-11-15 15:48:52 +0800

    :sparkles: update reducers

--- src/store/reducers/authSlice.ts
+++ src/store/reducers/authSlice.ts
@@ -2,18 +2,26 @@ import {createSlice} from '@reduxjs/toolkit'
 import type {PayloadAction} from '@reduxjs/toolkit'
 
 const initialState: authState = {
-  registerIsPass: false,
+  isAuth: false,
+  nativeRegisterIsPass: false,
 }
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setRegisterIsPass: (state, {payload}: PayloadAction<boolean>) => {
-      state.registerIsPass = payload
+    setIsAuth: (state, {payload}: PayloadAction<boolean>) => {
+      state.isAuth = payload
+    },
+    setNativeRegisterIsPass: (state, {payload}: PayloadAction<boolean>) => {
+      state.nativeRegisterIsPass = payload
+    },
+    setUserData: (state, {payload}: PayloadAction<UserDataType>) => {
+      state.userData = payload
     },
   },
 })
 
-export const {setRegisterIsPass} = authSlice.actions
+export const {setIsAuth, setNativeRegisterIsPass, setUserData} =
+  authSlice.actions
 export default authSlice.reducer
