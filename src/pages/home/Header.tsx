30857dc903e86603f4afe9ae303fe79ac6ef607b
Tsai-Chieh <d0177381@gmail.com>
2022-11-15 23:02:22 +0800

    :sparkles: update button content

--- src/pages/home/Header.tsx
+++ src/pages/home/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className='flex container'>
         <Logo />
         <div className='flex btns'>
-          <Button content={t('buttons.login')} style='second'
+          <Button content={t('buttons.login_or_register')} style='second'
             padding='10px 16px' />
           <Button content={t('buttons.history')} style='second'
             padding='10px 21px' />
