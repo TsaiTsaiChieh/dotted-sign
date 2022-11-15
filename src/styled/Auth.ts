bb90806c28e0c4eb8f6204a289321dc0cd836381
Tsai-Chieh <d0177381@gmail.com>
2022-11-15 23:05:11 +0800

    :art: format

--- src/styled/Auth.ts
+++ src/styled/Auth.ts
@@ -4,74 +4,73 @@ import {Color} from '../constants/Variables'
 import {AuthContainer} from './Modal'
 
 export const RegisterContainer = styled(AuthContainer)`
-width:420px;
-height:484px;
-padding: 40px 84px 210px;
-overflow: hidden;
+  width: 420px;
+  height: 484px;
+  padding: 40px 84px 210px;
+  overflow: hidden;
 `
 export const RegisterTitleWrap = styled.div`
-display: flex;
-align-items: center;
-gap: 13.75px;
+  display: flex;
+  align-items: center;
+  gap: 13.75px;
 `
 export const RegisterTitle = styled.h2`
-font-weight: bold;
-font-size: 36px;
+  font-weight: bold;
+  font-size: 36px;
 `
 export const RegisterFormWrap = styled.form`
-display: flex;
-flex-direction: column;
-gap:8px;
-margin: 30px 0 24px;
-position: relative;
-button {
-  margin-top:12px ;
-}
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin: 30px 0 24px;
+  position: relative;
+  button {
+    margin-top: 12px;
+  }
 `
 export const RegisterForm = styled.input`
-width: 358px;
-height: 42px;
-border: 3px solid ${Color.primary[50]};
-border-radius: 6px;
-text-align: left;
-padding-left: 10px;
-font-size: 16px;
-&::placeholder {
-color: ${Color.primary[50]};
-}
+  width: 358px;
+  height: 42px;
+  border: 3px solid ${Color.primary[50]};
+  border-radius: 6px;
+  text-align: left;
+  padding-left: 10px;
+  font-size: 16px;
+  &::placeholder {
+    color: ${Color.primary[50]};
+  }
 `
 export const FatBee = styled.img`
-content: url('/images/fat-bee-normal.svg');
-position: absolute;
-width: 100%;
-bottom: -10px;
+  content: url("/images/fat-bee-normal.svg");
+  position: absolute;
+  width: 100%;
+  bottom: -10px;
 `
 export const ThirdPartyWrap = styled.div`
-display: flex;
-gap: 48px;
+  display: flex;
+  gap: 48px;
 `
 export const GoogleIcon = styled.img`
-content: url('/images/google.svg');
-cursor: pointer;
+  content: url("/images/google.svg");
+  cursor: pointer;
 `
 export const FBIcon = styled.img`
-content: url('/images/fb.svg');
-cursor: pointer;
+  content: url("/images/fb.svg");
+  cursor: pointer;
 `
 export const LoginContainer = styled(RegisterContainer)`
-width:380px;
-height:492px;
-padding: 32px 104px 210px;
+  width: 380px;
+  height: 492px;
+  padding: 32px 104px 210px;
 `
 export const LoginTitleWrap = styled(RegisterTitleWrap)`
-flex-direction:column ;
+  flex-direction: column;
 `
 export const LoginTitle = styled(RegisterTitle)``
-export const LoginFormWrap = styled(RegisterFormWrap)`
-`
+export const LoginFormWrap = styled(RegisterFormWrap)``
 export const LoginForm = styled(RegisterForm)`
-border: 3px solid ${Color.blue[50]};
-&::placeholder {
-color: ${Color.blue[50]};
-}
+  border: 3px solid ${Color.blue[50]};
+  &::placeholder {
+    color: ${Color.blue[50]};
+  }
 `
