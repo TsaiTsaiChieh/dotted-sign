4abd1fe7e40af6bcf4f52f2886bd76be04f04cb5
Tsai-Chieh <d0177381@gmail.com>
2022-11-16 16:19:48 +0800

    :tada: init Docs styled-components

---
+++ src/styled/Docs.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const UploadIcon = styled.form`
+  width: 107px;
+  height: 107px;
+  cursor: pointer;
+  content: url("/images/upload-file.svg");
+`
