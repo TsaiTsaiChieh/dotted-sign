0e0158ebe8131d7fce1022ebd51a55cf1dd3e19e
Tsai-Chieh <d0177381@gmail.com>
2022-11-16 15:11:05 +0800

    :sparkles: add loose prop

--- src/components/layouts/Background.tsx
+++ src/components/layouts/Background.tsx
@@ -1,11 +1,22 @@
-const Background = () => {
+interface Props {
+  loose?: boolean
+}
+const Background = ({loose = false}: Props) => {
   return (
     <>
       <img className='bg top left' src='/images/bg-top-left.svg' alt='bg' />
-      <img className='bg bottom left' src='/images/bg-bottom-left.svg'
-        alt='bg' />
-      <img className='bg bottom right' src='/images/bg-bottom-right.svg'
-        alt='bg' />
+      <img
+        className='bg bottom left'
+        src={`/images/bg-bottom-left${loose ? '-empty' : ''}.svg`}
+        alt='bg'
+      />
+      {loose ? null : (
+        <img
+          className='bg bottom right'
+          src='/images/bg-bottom-right.svg'
+          alt='bg'
+        />
+      )}
     </>
   )
 }
