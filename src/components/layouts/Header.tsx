a63f18cc2b13a8e60209459f1c8e25be0149be67
Tsai-Chieh <d0177381@gmail.com>
2022-11-16 14:08:12 +0800

    :truck: mv Header and Background 2 layout folder

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,48 @@
+import {useTranslation} from 'react-i18next'
+
+import {useAppDispatch, useAppSelector} from '../../store/hook'
+import {logout} from '../../store/reducers/authSlice'
+import {setLoginVisible} from '../../store/reducers/uiSlice'
+import {Logo, Navbar, Photo} from '../../styled/Layout'
+import Button from '../Button'
+
+const Header = () => {
+  const dispatch = useAppDispatch()
+  const {t} = useTranslation()
+  const {isAuth, userData} = useAppSelector((state) => state.auth)
+  const openLoginModal = () => {
+    dispatch(setLoginVisible(true))
+  }
+  const handleLogout = () => {
+    dispatch(logout())
+  }
+  return (
+    <Navbar>
+      <div className='flex container'>
+        <Logo />
+        <div className='flex btns'>
+          {isAuth ? (
+            <Photo
+              path={userData!.photo === null ? undefined : userData!.photo}
+            />
+          ) : (
+            <Button
+              content={t('buttons.login_or_register')}
+              style='second'
+              padding='10px 16px'
+              onClick={openLoginModal}
+            />
+          )}
+          <Button
+            content={t('buttons.logout')}
+            style='second'
+            padding='10px 43px'
+            onClick={handleLogout}
+          />
+        </div>
+      </div>
+    </Navbar>
+  )
+}
+
+export default Header
