4cb997f55a8ec56edf0df02866457283c726df7e
Tsai-Chieh <d0177381@gmail.com>
2022-11-16 14:54:12 +0800

    :sparkles: import Navigation to map route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import {Routes, Route} from 'react-router-dom'
 
 import Home from '../pages/home/Home'
+import Main from '../pages/main/Main'
+import {Navigation} from './Navigation'
 import Protected from './Protected'
 
 const View = () => {
@@ -8,7 +10,13 @@ const View = () => {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route element={<Protected />}>
-        <></>
+        {Navigation.map((ele) => (
+          <Route
+            key={ele.id}
+            path={ele.path}
+            element={<Main childElement={ele.childElement} />}
+          />
+        ))}
       </Route>
     </Routes>
   )
