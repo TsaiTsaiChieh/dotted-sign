82ea908eaab6867fffd555353e3d94f617a472cf
Tsai-Chieh <d0177381@gmail.com>
2022-11-17 15:53:05 +0800

    :tada: init Canvas component

---
+++ src/pages/main/sign/Canvas.tsx
@@ -0,0 +1,13 @@
+import SignatureCanvas from 'react-signature-canvas'
+
+const Canvas = () => {
+  return (
+    <>
+      <SignatureCanvas
+        canvasProps={{className: 'signature'}}
+      />
+    </>
+  )
+}
+
+export default Canvas
