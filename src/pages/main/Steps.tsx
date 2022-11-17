4f3d3e220768a024ddec0265263519c41f5f554b
Tsai-Chieh <d0177381@gmail.com>
2022-11-17 15:17:11 +0800

    :sparkles: rm id props, should depend on stepId

--- src/pages/main/Steps.tsx
+++ src/pages/main/Steps.tsx
@@ -1,20 +1,28 @@
+import {useEffect} from 'react'
+
 import {useTranslation} from 'react-i18next'
 
+import {useAppDispatch, useAppSelector} from '../../store/hook'
+import {setStepId} from '../../store/reducers/uiSlice'
 import {StepWrap, Progress, StepNameWrap, StepName} from '../../styled/Main'
 
-interface Props {
-  id?: string
-}
-const Steps = ({id = '1'}: Props) => {
+const Steps = () => {
+  const dispatch = useAppDispatch()
+  const {stepId} = useAppSelector((state) => state.ui)
+  const {docBase64} = useAppSelector((state) => state.persist)
   const {t} = useTranslation()
   const stepNames: string[] = t('steps', {returnObjects: true})
-  return (
+  useEffect(() => {
+    dispatch(setStepId('2'))
+    if (!docBase64) dispatch(setStepId('1'))
+  }, [docBase64])
 
+  return (
     <StepWrap>
       <StepNameWrap>
         {stepNames.map((ele, i) => (<StepName key={i}>{ele}</StepName>))}
       </StepNameWrap>
-      <Progress id={id} />
+      <Progress id={stepId} />
     </StepWrap>
 
   )
