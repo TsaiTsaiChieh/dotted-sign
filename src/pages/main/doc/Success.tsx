24ab18caef71ef0f621c2780a9b569ac0acbd7a4
Tsai-Chieh <d0177381@gmail.com>
2022-11-17 15:18:26 +0800

    :tada: init Success component in Doc

---
+++ src/pages/main/doc/Success.tsx
@@ -0,0 +1,31 @@
+import {useTranslation} from 'react-i18next'
+import {useNavigate} from 'react-router-dom'
+
+import {Navigation} from '../../../routes/Navigation'
+import {useAppDispatch} from '../../../store/hook'
+import {setFile} from '../../../store/reducers/persistSlice'
+import {setStepId} from '../../../store/reducers/uiSlice'
+import {HappyIcon, SuccessWrap} from '../../../styled/Docs'
+import PrevNextBtns from '../PrevNextBtns'
+
+const Success = () => {
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const {t} = useTranslation()
+  const prevAction = () => {
+    dispatch(setFile(undefined))
+  }
+  const nextAction = () => {
+    dispatch(setStepId('3'))
+    navigate(Navigation[1].path)
+  }
+  return (
+    <SuccessWrap>
+      <HappyIcon />
+      <span>{t('hints.upload_success')}</span>
+      <PrevNextBtns prevAction={prevAction} nextAction={nextAction} />
+    </SuccessWrap>
+  )
+}
+
+export default Success
