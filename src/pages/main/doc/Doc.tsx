757757cb849f403c8e3f6b516765f0919c5afc47
Tsai-Chieh <d0177381@gmail.com>
2022-11-17 15:17:43 +0800

    :sparkles: import UploadDocForm and rm Button

--- src/pages/main/doc/Doc.tsx
+++ src/pages/main/doc/Doc.tsx
@@ -1,13 +1,12 @@
 import {useTranslation} from 'react-i18next'
 
-import Button from '../../../components/Button'
 import {
   InteractCard,
   SubTitle,
   Title,
   TitleWrap,
 } from '../../../styled/Main'
-import UploadForm from './UploadForm'
+import UploadDocForm from './UploadDocForm'
 
 const Doc = () => {
   const {t} = useTranslation()
@@ -18,8 +17,7 @@ const Doc = () => {
         <Title>{t('titles.upload_file')}</Title>
         <SubTitle>{t('titles.start_to_record')}</SubTitle>
       </TitleWrap>
-      <UploadForm />
-      <Button content={t('buttons.upload_file')} padding='10px 130px' />
+      <UploadDocForm />
     </InteractCard>
   )
 }
