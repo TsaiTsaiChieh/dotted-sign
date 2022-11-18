d93c4ac2252ff1420fcd9c57019ce359a2e8ea69
Tsai-Chieh <d0177381@gmail.com>
2022-11-18 21:15:08 +0800

    :lipstick: add UIWrap, UndoIcon and Thickness

--- src/styled/Sign.ts
+++ src/styled/Sign.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
 
+import {Color} from '../constants/Variables'
+
 export const SignatureWrap = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
 `
-export const ColorInput = styled.input``
-export const ThicknessInput = styled.input``
-export const ClearBtnWrap = styled.div`
+export const UIWrap = styled.div`
   display: flex;
   cursor: pointer;
   align-items: center;
@@ -17,7 +17,33 @@ export const ClearBtnWrap = styled.div`
     font-weight: bold;
   }
 `
-export const ClearIcon = styled.img`
+export const Thickness = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${Color.blue[50]};
+  padding: 0 3px;
+  border-radius: 6px;
+  width: 40px;
+  height: 30px;
+  color: ${Color.white[50]};
+`
+export const ThicknessInput = styled.input`
+  width: 100%;
+`
+export const BtnWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const UndoIcon = styled.img`
+  content: url("/images/undo.svg");
+  width: 20px;
+  height: 20px;
+  padding: 5px;
+  background: ${Color.blue[50]};
+  border-radius: 50%;
+`
+export const ClearIcon = styled(UndoIcon)`
   content: url("/images/clear.svg");
 `
 export const HappyIcon = styled.img`
