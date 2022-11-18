c8f2a77fd89066cfa12daf02d596022b7ad0ed6c
Tsai-Chieh <d0177381@gmail.com>
2022-11-18 15:50:10 +0800

    :sparkles: import react-pdf css

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,7 @@
 import i18n from 'i18next'
 import {initReactI18next} from 'react-i18next'
 
+import 'react-pdf/dist/esm/Page/TextLayer.css'
 import LocaleZhCN from '../assets/locales/zh.json'
 
 const defaultNS = 'zh'
