972bab20205c0568bd35f759f284f49bb18523df
Tsai-Chieh <d0177381@gmail.com>
2022-11-18 15:52:19 +0800

    :sparkles: add other component and import Preview1

--- src/pages/main/download/Download.tsx
+++ src/pages/main/download/Download.tsx
@@ -1,6 +1,18 @@
+import {useTranslation} from 'react-i18next'
+
+import {InteractCard, TitleWrap, Title, SubTitle} from '../../../styled/Main'
+import Preview1 from './Preview1'
+
 const Download = () => {
+  const {t} = useTranslation()
   return (
-    <div>Download</div>
+    <InteractCard>
+      <TitleWrap>
+        <Title>{t('titles.sign_it')}</Title>
+        <SubTitle>{t('titles.move_sign')}</SubTitle>
+      </TitleWrap>
+      <Preview1 />
+    </InteractCard>
   )
 }
 
