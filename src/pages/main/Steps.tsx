9b8da2b7d5afb56e5d48b0172e5e5edb9205851d
Tsai-Chieh <d0177381@gmail.com>
2022-11-19 20:49:37 +0800

    :recycle: add location dependency and format

--- src/pages/main/Steps.tsx
+++ src/pages/main/Steps.tsx
@@ -1,7 +1,9 @@
 import {useEffect} from 'react'
 
 import {useTranslation} from 'react-i18next'
+import {useLocation} from 'react-router-dom'
 
+import {Navigation} from '../../routes/Navigation'
 import {useAppDispatch, useAppSelector} from '../../store/hook'
 import {setStepId} from '../../store/reducers/uiSlice'
 import {StepWrap, Progress, StepNameWrap, StepName} from '../../styled/Main'
@@ -12,23 +14,28 @@ const Steps = () => {
   const {docBase64, signBase64} = useAppSelector((state) => state.persist)
   const {t} = useTranslation()
   const stepNames: string[] = t('steps', {returnObjects: true})
+  const location = useLocation()
   useEffect(() => {
-    dispatch(setStepId('2'))
-    if (!docBase64) dispatch(setStepId('1'))
-  }, [docBase64])
-  useEffect(() => {
-    dispatch(setStepId('4'))
-    if (!signBase64) dispatch(setStepId('3'))
-  }, [signBase64])
+    if (location.pathname === Navigation[0].path && !docBase64) {
+      dispatch(setStepId('1'))
+    } else if (location.pathname === Navigation[0].path && docBase64) {
+      dispatch(setStepId('2'))
+    } else if (location.pathname === Navigation[1].path && !signBase64) {
+      dispatch(setStepId('3'))
+    } else if (location.pathname === Navigation[1].path && signBase64) {
+      dispatch(setStepId('4'))
+    }
+  }, [location, docBase64, signBase64])
 
   return (
     <StepWrap>
       <StepNameWrap>
-        {stepNames.map((ele, i) => (<StepName key={i}>{ele}</StepName>))}
+        {stepNames.map((ele, i) => (
+          <StepName key={i}>{ele}</StepName>
+        ))}
       </StepNameWrap>
       <Progress id={stepId} />
     </StepWrap>
-
   )
 }
 
