bfa970522597709c7c0de421f41e0386d0530aff
Tsai-Chieh <d0177381@gmail.com>
2022-11-21 00:36:26 +0800

    :sparkles: add when docBase64 is image type logic (should be fix) and setStepId action

--- src/pages/main/download/Preview.tsx
+++ src/pages/main/download/Preview.tsx
@@ -7,18 +7,23 @@ import {pdfjs} from 'react-pdf'
 
 import Button from '../../../components/Button'
 import {useAppSelector} from '../../../store/hook'
+import {useAppDispatch} from '../../../store/hook/index'
+import {setStepId} from '../../../store/reducers/uiSlice'
 import {CanvasContainer} from '../../../styled/Download'
 import {base64ToArrayBuffer, getPDFCanvas} from '../../../utils/helper'
 pdfjs.GlobalWorkerOptions.workerSrc =
   `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 const Preview = () => {
-  const signSize = 100
-  const [docWidth, setDocWidth] = useState<number>(400)
-  const [docHeight, setDocHeight] = useState<number>(400)
+  const dispatch = useAppDispatch()
+  const signSize = 200
+  const [docWidth, setDocWidth] = useState<number>(750)
+  const [docHeight, setDocHeight] = useState<number>(850)
   const mainRef = useRef(null)
   const {t} = useTranslation()
-  const {signBase64, docBase64} = useAppSelector((state) => state.persist)
+  const {signBase64, docBase64, docIsPDF} = useAppSelector(
+    (state) => state.persist,
+  )
   const [canvas, setCanvas] = useState<any | null>(null)
   useEffect(() => {
     const c = new fabric.Canvas(mainRef.current)
@@ -34,21 +39,39 @@ const Preview = () => {
       })
     }
   }, [canvas, signBase64])
+
   // pdf
   useEffect(() => {
     if (canvas && docBase64) {
-      const arrBuffer = base64ToArrayBuffer(docBase64)
-      const updateCanvas = async () => {
-        const pdfCanvas = await getPDFCanvas(arrBuffer)
-        const pdfImg = new fabric.Image(pdfCanvas)
-        const {width, height} = pdfImg
-        setDocWidth(width!)
-        setDocHeight(height!)
-        canvas.setWidth(width!)
-        canvas.setHeight(height!)
-        canvas.setBackgroundImage(pdfImg).renderAll()
+      // pdf
+      if (docIsPDF) {
+        const arrBuffer = base64ToArrayBuffer(docBase64)
+        const updateCanvas = async () => {
+          const pdfCanvas = await getPDFCanvas(arrBuffer)
+          const pdfImg = new fabric.Image(pdfCanvas)
+          const {width, height} = pdfImg
+          setDocWidth(width!)
+          setDocHeight(height!)
+          canvas.setWidth(width!)
+          canvas.setHeight(height!)
+          canvas.setBackgroundImage(pdfImg).renderAll()
+        }
+        updateCanvas()
+      } else {
+        // image
+        // FIXME when the signature be clicked will render
+        const image = new Image()
+        image.src = docBase64
+        image.onload = () => {
+          fabric.Image.fromURL(docBase64, (img) => {
+            const {width, height} = img
+            console.log('ok')
+            canvas.setHeight(height)
+            canvas.setWidth(width)
+            canvas.setBackgroundImage(docBase64).renderAll()
+          })
+        }
       }
-      updateCanvas()
     }
   }, [canvas, docBase64])
   // download
@@ -59,6 +82,7 @@ const Preview = () => {
     const height = doc.internal.pageSize.getHeight()
     doc.addImage(divImage, 'PNG', 0, 0, width, height)
     doc.save('download.pdf')
+    dispatch(setStepId('6'))
   }
 
   return (
