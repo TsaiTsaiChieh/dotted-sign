b19e0767f8bcd8f7ce07abb6e135b5519c1daca8
Tsai-Chieh <d0177381@gmail.com>
2022-11-21 01:43:03 +0800

    :tada: init FbLogin component

---
+++ src/pages/auth/FbLogin.tsx
@@ -0,0 +1,22 @@
+import {createData} from '../../services/curd'
+import {fbLogin} from '../../services/user'
+import {useAppDispatch} from '../../store/hook/index'
+import {login} from '../../store/reducers/authSlice'
+import {setLoginVisible, setRegisterVisible} from '../../store/reducers/uiSlice'
+import {FBIcon} from '../../styled/Auth'
+
+const FbLogin = () => {
+  const dispatch = useAppDispatch()
+  const handleLogin = async () => {
+    const user = await fbLogin()
+    await createData('user', user.email, user)
+    dispatch(login(user))
+    dispatch(setLoginVisible(false))
+    dispatch(setRegisterVisible(false))
+  }
+  return (
+    <FBIcon onClick={handleLogin} />
+  )
+}
+
+export default FbLogin
