2c190a0316c2d75ba7e1cf696b34ffa8176fa415
Tsai-Chieh <d0177381@gmail.com>
2022-11-28 10:03:55 +0800

    :sparkles: add loading

--- src/pages/auth/FbLogin.tsx
+++ src/pages/auth/FbLogin.tsx
@@ -2,17 +2,23 @@ import {createData} from '../../services/curd'
 import {fbLogin} from '../../services/user'
 import {useAppDispatch} from '../../store/hook/index'
 import {login} from '../../store/reducers/authSlice'
-import {setLoginVisible, setRegisterVisible} from '../../store/reducers/uiSlice'
+import {
+  setLoading,
+  setLoginVisible,
+  setRegisterVisible,
+} from '../../store/reducers/uiSlice'
 import {FBIcon} from '../../styled/Auth'
 
 const FbLogin = () => {
   const dispatch = useAppDispatch()
   const handleLogin = async () => {
     const user = await fbLogin()
+    dispatch(setLoading(true))
     await createData('users', user.email, user)
     dispatch(login(user))
     dispatch(setLoginVisible(false))
     dispatch(setRegisterVisible(false))
+    dispatch(setLoading(false))
   }
   return (
     <FBIcon onClick={handleLogin} />
--- src/pages/auth/GoogleLogin.tsx
+++ src/pages/auth/GoogleLogin.tsx
@@ -2,22 +2,26 @@ import {createData} from '../../services/curd'
 import {googleLogin} from '../../services/user'
 import {useAppDispatch} from '../../store/hook'
 import {login} from '../../store/reducers/authSlice'
-import {setLoginVisible, setRegisterVisible} from '../../store/reducers/uiSlice'
+import {
+  setLoading,
+  setLoginVisible,
+  setRegisterVisible,
+} from '../../store/reducers/uiSlice'
 import {GoogleIcon} from '../../styled/Auth'
 
 const GoogleLogin = () => {
   const dispatch = useAppDispatch()
   const handleLogin = async () => {
     const user = await googleLogin()
+    dispatch(setLoading(true))
     await createData('users', user.email, user)
     dispatch(login(user))
     dispatch(setLoginVisible(false))
     dispatch(setRegisterVisible(false))
+    dispatch(setLoading(false))
   }
 
-  return (
-    <GoogleIcon onClick={handleLogin} />
-  )
+  return <GoogleIcon onClick={handleLogin} />
 }
 
 export default GoogleLogin
